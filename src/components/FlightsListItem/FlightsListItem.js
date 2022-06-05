cb57f3f4246d83e89b0f583fb50d82bc8020db0c
karolinaskorupska <pixie22@op.pl>
2022-06-05 20:33:37 +0200

    api works

--- src/components/FlightsListItem/FlightsListItem.js
+++ src/components/FlightsListItem/FlightsListItem.js
@@ -3,8 +3,6 @@ import PropTypes from 'prop-types';
 import { Element, FlightMainPhoto, FlightInfo, FlightNumber, FlightName } from './FlightsListItem.styled';
 import ButtonMore from 'components/buttons/ButtonMore';
 
-const showIndex = (index) => alert(`This is a flight nr #${index + 1}`);
-
 const FlightsListItem = ({ index, flightData }) => {
   const flightName = flightData.name;
   const flightDate = flightData.static_fire_date_utc;
@@ -20,7 +18,7 @@ const FlightsListItem = ({ index, flightData }) => {
         <FlightNumber className="flightNumber">{flightNumber}</FlightNumber>
         <FlightName className="flightName">{flightName}</FlightName>
         <div className="flightDate">{flightDate}</div>
-        <ButtonMore onClick={() => showIndex(index)} />
+        <ButtonMore />
       </FlightInfo>
     </Element>
   );
